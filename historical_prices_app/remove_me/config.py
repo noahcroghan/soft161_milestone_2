dc861cb875e42a758ba107d7e4bdfd98bf0c00f7
Noah Croghan <ncroghan2@huskers.unl.edu>
2025-05-06 12:41:52 -0500

    Cleanup unnecessary/unused files in historical prices app

--- historical_prices_app/remove_me/config.py
+++
@@ -1,5 +0,0 @@
-password = ""  # Put your password between these quotes.
-
-username = "root"  # Only change this if you need to
-
-port = 3306  # Only change this if your MySQL server is configured to use a different port
