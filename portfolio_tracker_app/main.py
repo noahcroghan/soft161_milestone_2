353d6fbe638029edf5f72434c25ebc7c1907ab0e
Noah Croghan <ncroghan2@huskers.unl.edu>
2025-05-07 10:30:25 -0500

    Revert new crypto screen being deleted

--- portfolio_tracker_app/main.py
+++ portfolio_tracker_app/main.py
@@ -1,56 +1,9 @@
-# from kivy.app import App
-# from kivy.modules import inspector
-# from kivy.core.window import Window
-# from kivy.properties import NumericProperty, StringProperty
-#
-#
-# class PortfolioTrackerApp(App):
-#     crypto_price = StringProperty("")
-#     portfolio_value = StringProperty("")
-#     portfolio_value_change = StringProperty("")
-#     input_text = StringProperty("")
-#
-#     def check_value(self, input_id, message_id):
-#         if input_id.text != '':
-#             message_id.text = ""
-#             self.crypto_price = "$#.##"
-#             self.portfolio_value = "$###.##"
-#             self.portfolio_value_change = "+ ##.##%"
-#         else:
-#             self.crypto_price = ""
-#             self.portfolio_value = ""
-#             self.portfolio_value_change = ""
-#             message_id.text = "Portfolio ID is Required"
-#
-#     def go_to_home(self, message_id):
-#         self.crypto_price = ""
-#         self.portfolio_value = ""
-#         self.portfolio_value_change = ""
-#         self.input_text = " "
-#         self.input_text = ""
-#         message_id.text = ""
-#
-#     def submit_data(self, message_id, id1, id2, id3, message_pass, message_fail):
-#         if id1.text != '' and id2.text != '' and id3.text != '':
-#             message_id.text = message_pass
-#             id1.text = ""
-#             id2.text = ""
-#             id3.text = ""
-#         else:
-#            message_id.text = message_fail
-#
-#     def build(self):
-#         inspector.create_inspector(Window, self)
-#
-#
-# if __name__ == '__main__':
-#     Window.size = (420, 720)
-#     app = PortfolioTrackerApp()
-#     app.run()
 from datetime import datetime
 
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
+from sqlalchemy.exc import SQLAlchemyError
+
 from installer.database import CryptoDatabase, User, Cryptocurrency, Portfolio  # Adjust path if needed
 from installer.database_installer import coin_gecko_api
 
@@ -68,7 +21,79 @@ def get_all_portfolios():
     return str_list
 
 class NewCryptoScreen(Screen):
-    pass
+    def submit_new_crypto(self):
+        name = self.ids.new_crypto_name_input.text.strip().capitalize()
+        symbol = self.ids.new_crypto_symbol_input.text.strip().upper()
+        price = self.ids.new_crypto_price_input.text
+        percent_change_24h = self.ids.new_crypto_percent_change_24h_input.text
+        if not name or len(name) > 50:
+            self.show_error("Name field is required and must be less than 50 characters long.")
+            return
+        if not symbol or len(symbol) > 15:
+            self.show_error("Symbol field is required and must be less than 15 characters long.")
+            return
+        if " " in symbol:
+            self.show_error("Symbol field cannot contain spaces.")
+            return
+        try:
+            price = float(price)
+            if price < 0:
+                self.show_error("Price cannot be negative.")
+                return
+            elif price > 1000000000000:
+                self.show_error("Price cannot be greater than $10,000,000,000.")
+                return
+        except ValueError:
+            self.show_error("Price field is required.")
+            return
+
+        try:
+            percent_change_24h = float(percent_change_24h)
+            if percent_change_24h < -100 or percent_change_24h > 1000:
+                self.show_error("Percent change must be between -100% and 1000%.")
+                return
+        except ValueError:
+            self.show_error("Percent change field is required.")
+            return
+
+        try:
+            session = CryptoDatabase.get_session()
+            crypto_exists = session.query(Cryptocurrency).filter(
+                (Cryptocurrency.symbol == symbol) | (Cryptocurrency.name == name)).first()
+
+            if crypto_exists:
+                self.show_error("Submitted crypto already exists.")
+                return
+
+            new_crypto = Cryptocurrency(name=name, symbol=symbol, current_price=price,
+                                        percent_change_24h=percent_change_24h, coingecko_id=symbol)
+            # TODO: Maybe the symbol should have some kind of indication that it is not a CoinGecko coin
+            #  Currently, it's not nullable
+            session.add(new_crypto)
+            session.commit()
+
+            self.show_success(f"New crypto '{name}' [i]({symbol})[/i] added successfully.")
+        except SQLAlchemyError as sql_alchemy_error:
+            self.show_error(f'General Database error:\n{sql_alchemy_error}')
+        except Exception as exception:
+            self.show_error(f'General error occurred:\n{exception}')
+
+    def show_error(self, message):
+        self.ids.new_crypto_message.text = message
+        self.ids.new_crypto_message.color = ((214 / 256), (69 / 256), (69 / 256), 1.0)
+
+    def show_success(self, message):
+        self.ids.new_crypto_message.text = message
+        self.ids.new_crypto_message.color = ((50 / 256), (222 / 256), (153 / 256), 1.0)
+
+    def on_leave(self):
+        self.ids.new_crypto_message.text = ""
+        self.ids.new_crypto_name_input.text = ""
+        self.ids.new_crypto_symbol_input.text = ""
+        self.ids.new_crypto_price_input.text = ""
+        self.ids.new_crypto_percent_change_24h_input.text = ""
+
+
 
 class NewPortfolioScreen(Screen):
 
