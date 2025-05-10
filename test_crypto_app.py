1f3185a5e962e4d25968aab7d12e3a5203f070ec
bchebefuh2 <bchebefuh2@huskers.unl.edu>
2025-05-09 23:09:08 -0500

    Refactored unit tests file.

--- test_crypto_app.py
+++ test_crypto_app.py
@@ -1,11 +1,8 @@
 from datetime import datetime
-from tkinter.font import names
 from unittest import TestCase
 
-from requests import session
-
 from installer.database import CryptoDatabase, Persisted, User, Cryptocurrency, Portfolio
-from portfolio_tracker_app.main import NewCryptoScreen, add_crypto_to_database, add_portfolio_to_database
+from portfolio_tracker_app.main import add_crypto_to_database, add_portfolio_to_database
 
 
 class TestCryptoApp(TestCase):
@@ -14,23 +11,23 @@ class TestCryptoApp(TestCase):
         Persisted.metadata.create_all(self.db.engine)
         self.session = self.db.create_session()
 
-    #Test for getting all the usernames method from the database
+    # Test for getting all the usernames method from the database
     def test_get_all_usernames_returns_multiple_users(self):
-        self.db.create_user('Kenny',self.session)
-        self.db.create_user('Austin',self.session)
+        self.db.create_user('Kenny', self.session)
+        self.db.create_user('Austin', self.session)
         usernames = self.db.get_all_usernames(self.session)
-        self.assertIn('Kenny',usernames)
-        self.assertIn('Austin',usernames)
+        self.assertIn('Kenny', usernames)
+        self.assertIn('Austin', usernames)
 
-    #Test for creating new users method to the database
+    # Test for creating new users method to the database
     def test_create_user_and_insert_in_database(self):
-        self.db.create_user('Deku',self.session)
-        user = self.session.query(User).filter(User.user_name=='Deku').one()
-        self.assertEqual(user.user_name,'Deku')
+        self.db.create_user('Deku', self.session)
+        user = self.session.query(User).filter(User.user_name == 'Deku').one()
+        self.assertEqual(user.user_name, 'Deku')
 
-    #Test inserting adding new crypto to the database
+    # Test inserting adding new crypto to the database
     def test_add_crypto_to_database_successfully(self):
-        example_crypto= add_crypto_to_database(
+        example_crypto = add_crypto_to_database(
             session=self.session,
             name="Capstone",
             symbol="CSC",
@@ -40,43 +37,39 @@ class TestCryptoApp(TestCase):
         self.assertTrue(example_crypto)
 
         inserted = self.session.query(Cryptocurrency).filter_by(symbol='CSC').one()
-        self.assertEqual(inserted.name,"Capstone")
+        self.assertEqual(inserted.name, "Capstone")
 
     def test_add_crypto_to_database_validates_all_fields(self):
         name = "CheckCoin"
-        symbol ="CC"
+        symbol = "CC"
         price = 123.12
         percent_change_24h = 9.99
 
-        crypto=add_crypto_to_database(
-            session =self.session,
+        crypto = add_crypto_to_database(
+            session=self.session,
             name=name,
             symbol=symbol,
             price=price,
             percent_change_24h=percent_change_24h
         )
         self.assertTrue(crypto)
-        inserted= self.session.query(Cryptocurrency).filter_by(symbol=symbol).one()
-        self.assertEqual(inserted.name,name)
+        inserted = self.session.query(Cryptocurrency).filter_by(symbol=symbol).one()
+        self.assertEqual(inserted.name, name)
         self.assertEqual(float(inserted.current_price), price)
         self.assertEqual(float(inserted.percent_change_24h), percent_change_24h)
 
     def test_add_portfolio_to_database_successfully(self):
-
-        example_portfolio= add_portfolio_to_database(
-            session = self.session,
-            user_id = 1,
-            crypto_id = 1,
-            coin_amount = 24,
-            purchase_date = datetime(2025,2,6).date(),
-            initial_investment_amount = 28
-            )
+        example_portfolio = add_portfolio_to_database(
+            session=self.session,
+            user_id=1,
+            crypto_id=1,
+            coin_amount=24,
+            purchase_date=datetime(2025, 2, 6).date(),
+            initial_investment_amount=28
+        )
         self.assertTrue(example_portfolio)
-        inserted = self.session.query(Portfolio).filter_by(purchase_date=datetime(2025,2,6).date()).one()
-        self.assertEqual(inserted.coin_amount,24)
+        inserted = self.session.query(Portfolio).filter_by(purchase_date=datetime(2025, 2, 6).date()).one()
+        self.assertEqual(inserted.coin_amount, 24)
 
     def tearDown(self):
         self.session.close()
-
-
-
